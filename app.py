1c887e3c89f3b9fe8992009915e919a8432ed4f3
David Rojas <colombianclimber@gmail.com>
2020-05-31 12:46:38 -0700

    mroe stuff

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from fourbp import app
-from flask import render_template
+from fourbp import app, mail
+from fourbp.forms import ContactUsForm
+from flask import render_template, session, request, redirect, url_for, flash, abort
+from flask_login import login_user, login_required, logout_user
+from flask_mail import Message
+from werkzeug.utils import secure_filename
+import os
 
 @app.route('/')
 def index():
@@ -13,11 +18,36 @@ def products():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    
+    form = ContactUsForm()
+    
+    if form.validate_on_submit():
+        session['first'] = form.first.data
+        session['last'] = form.last.data
+        session['email'] = form.email.data
+        session['quantity'] = form.quantity.data
+        session['dateby'] = form.dateby.data
+        session['message'] = form.message.data
+        msg = Message('New FourBPrints email', recipients=['fourbprints@gmail.com'], reply_to=session['email'])
+        msg.body = session['message']
+        if form.file.data != None:
+            file = form.file.data
+            filename = secure_filename(form.file.data.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as fp:
+              msg.attach(filename, "image/png", fp.read())
+        mail.send(msg)
+        
+        flash('Thanks for the Message! We will get back to you soon!')
+        return redirect(url_for('thankyou'))
+    
+    return render_template('contact.html', form=form)
 
-
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
 
 
 
---
+++ fourbp/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, PasswordField, BooleanField, DateTimeField,
+                     RadioField, SelectField, TextField, 
+                     TextAreaField, SubmitField, IntegerField, FileField)
+from wtforms.validators import DataRequired, InputRequired, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import ValidationError
+
+class ContactUsForm(FlaskForm):
+    first = StringField('First Name:', validators=[DataRequired()])
+    last = StringField('Last Name:', validators=[DataRequired()])
+    email = StringField('Email:', validators=[DataRequired(), Email()])
+    quantity = StringField('Approx Quantity:')
+    message = TextAreaField('Enter Your Message:', validators=[DataRequired()])
+    dateby = DateTimeField('Date Needed by:')
+    file = FileField('Upload a file to send us', validators=[FileAllowed(['png', 'jpg', 'gif'], 'Invalid file type')])
+    submit = SubmitField('SEND')
