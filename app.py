7e934f046c39075d5607fcb5cb0e925749998964
Ayoacatlcincin Mina Rojas <Mina.Rojas1311@gmail.com>
2020-05-31 11:31:22 -0700

    stuff

---
+++ app.py
@@ -0,0 +1,25 @@
+from fourbp import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
